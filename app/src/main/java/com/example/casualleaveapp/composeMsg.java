ce0932aebec454638fe7720e3dec3563add68a5a
MrVineetPatil <vbpatil9980@gmail.com>
2019-11-20 18:09:19 +0530

    add calendar

--- app/src/main/java/com/example/casualleaveapp/composeMsg.java
+++ app/src/main/java/com/example/casualleaveapp/composeMsg.java
@@ -1,7 +1,15 @@
 package com.example.casualleaveapp;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class composeMsg extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class composeMsg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_msg);
+        Button addD = findViewById(R.id.addDate);
+        addD.setOnClickListener(new View.OnClickListener() {
+
+            String date;
+            DatePickerDialog datePickerDialog;
+            int year;
+            int month;
+            int dayOfMonth;
+            Calendar calendar;
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(composeMsg.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                date = day + "/" + (month + 1) + "/" + year;
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
     }
 }
