84f23fdef33053adc6366f143711af42480e9945
MrVineetPatil <vbpatil9980@gmail.com>
2019-11-20 17:52:33 +0530

    compose

---
+++ app/src/main/java/com/example/casualleaveapp/GalleryAdapter.java
@@ -0,0 +1,85 @@
+package com.example.casualleaveapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CalendarView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class GalleryAdapter extends BaseAdapter {
+
+    private Context ctx;
+    private int pos;
+    private LayoutInflater inflater;
+    private CalendarView ivGallery;
+    ArrayList<Uri> mArrayUri;
+    public GalleryAdapter(Context ctx, ArrayList<Uri> mArrayUri) {
+
+        this.ctx = ctx;
+        this.mArrayUri = mArrayUri;
+    }
+
+    @Override
+    public int getCount() {
+        return mArrayUri.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mArrayUri.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        pos = position;
+        inflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        @SuppressLint("ViewHolder") View itemView = inflater.inflate(R.layout.dateitem, parent, false);
+
+        /*ivGallery = (CalendarView) itemView.findViewById(R.id.calendarView);
+
+        ivGallery.setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
+
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth) {
+
+                                // Store the value of date with
+                                // format in String type Variable
+                                // Add 1 in month because month
+                                // index is start with 0
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+
+                                // set this date in TextView for Display
+                                date_view.setText(Date);
+                            }
+                        });*/
+
+        return itemView;
+    }
+
+
+}
--- app/src/main/java/com/example/casualleaveapp/teacherPage.java
+++ app/src/main/java/com/example/casualleaveapp/teacherPage.java
@@ -1,7 +1,10 @@
 package com.example.casualleaveapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class teacherPage extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class teacherPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_page);
+        Button appleave = findViewById(R.id.applyleave);
+        appleave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(teacherPage.this,composeMsg.class));
+            }
+        });
     }
 }
