40623dbc9adc45195b326aaa85846cb3f6f00b82
MrVineetPatil <vbpatil9980@gmail.com>
2019-11-24 15:49:25 +0530

    timetable

--- app/src/main/java/com/example/casualleaveapp/teacherPage.java
+++ app/src/main/java/com/example/casualleaveapp/teacherPage.java
@@ -13,10 +13,17 @@ public class teacherPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_page);
         Button appleave = findViewById(R.id.applyleave);
+        Button appTime = findViewById(R.id.myTIME);
         appleave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(teacherPage.this,composeMsg.class));
+            }
+        });
+        appTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(teacherPage.this,MainActivity.class));
             }
         });
     }
